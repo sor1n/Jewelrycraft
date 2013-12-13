9ae4d9bcda4cce0b1398b1741da32ca3c3c412ae
OnyxDarkKnight <sor1n.iliutza16@gmail.com>
2013-12-13 23:18:30 +0200

    First commit by me

--- common/bspkrs/briefcasespeakers/CommonProxy.java
+++ common/bspkrs/briefcasespeakers/CommonProxy.java
@@ -1,6 +1,24 @@
 package bspkrs.briefcasespeakers;
 
-public class CommonProxy
-{   
-    
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import cpw.mods.fml.common.network.IGuiHandler;
+
+public class CommonProxy implements IGuiHandler
+{
+
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
---
+++ common/bspkrs/briefcasespeakers/item/ItemThiefGloves.java
@@ -0,0 +1,12 @@
+package bspkrs.briefcasespeakers.item;
+
+import net.minecraft.item.Item;
+
+public class ItemThiefGloves extends Item
+{
+    public ItemThiefGloves(int par1)
+    {
+        super(par1);
+    }
+    
+}
