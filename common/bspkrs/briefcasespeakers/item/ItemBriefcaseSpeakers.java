eb280ff9eae856cf02808b9bb97c91c280e0fb06
OnyxDarkKnight <sor1n.iliutza16@gmail.com>
2013-12-14 00:15:58 +0200

    Working on the items

--- common/bspkrs/briefcasespeakers/item/ItemBriefcaseSpeakers.java
+++ common/bspkrs/briefcasespeakers/item/ItemBriefcaseSpeakers.java
@@ -1,5 +1,8 @@
 package bspkrs.briefcasespeakers.item;
 
+import bspkrs.briefcasespeakers.BriefcaseSpeakersMod;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class ItemBriefcaseSpeakers extends Item
@@ -7,6 +10,12 @@ public class ItemBriefcaseSpeakers extends Item
     public ItemBriefcaseSpeakers(int par1)
     {
         super(par1);
+        setCreativeTab(CreativeTabs.tabMaterials); 
     }
     
+    public void registerIcons(IconRegister reg) 
+    { 
+        if (itemID == BriefcaseSpeakersMod.bspkrs.itemID);
+        this.itemIcon = reg.registerIcon("amethyst");
+    }    
 }
--- common/bspkrs/briefcasespeakers/item/ItemThiefGloves.java
+++ common/bspkrs/briefcasespeakers/item/ItemThiefGloves.java
@@ -1,12 +1,34 @@
 package bspkrs.briefcasespeakers.item;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Tuple;
 
 public class ItemThiefGloves extends Item
 {
     public ItemThiefGloves(int par1)
     {
         super(par1);
+        this.setCreativeTab(CreativeTabs.tabTools);
+    }
+    
+    public boolean itemInteractionForEntity(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, EntityLivingBase par3EntityLivingBase)
+    {
+        if (par3EntityLivingBase instanceof EntityVillager)
+        {
+            EntityVillager entityliving = (EntityVillager)par3EntityLivingBase;
+            Tuple tuple = (Tuple)entityliving.villagerStockList.get(Integer.valueOf(1));
+            entityliving.dropItem(((Integer)tuple.getFirst()).intValue(), 1);
+            return true;
+        }
+        else
+        {
+            return super.itemInteractionForEntity(par1ItemStack, par2EntityPlayer, par3EntityLivingBase);
+        }
     }
     
 }
