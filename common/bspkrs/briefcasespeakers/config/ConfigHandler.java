d2122cd45973fe93fb80b2d0ab5338cc9de8d289
bspkrs <bspkrs@gmail.com>
2013-12-14 13:29:13 -0500

    changed briefcase speakers ID to be a block (duh)

--- common/bspkrs/briefcasespeakers/config/ConfigHandler.java
+++ common/bspkrs/briefcasespeakers/config/ConfigHandler.java
@@ -7,10 +7,11 @@ public class ConfigHandler
 {
     private static Configuration config;
     public static int            idThiefGloves       = 17493;
-    public static int            idBriefcaseSpeakers = 17494;
-    public static int            idSpeaker           = 17495;
-    public static int            idRemote            = 17496;
-    public static int            idShadowIngot         = 17497;
+    public static int            idSpeaker           = 17494;
+    public static int            idRemote            = 17495;
+    public static int            idShadowIngot       = 17496;
+    
+    public static int            idBriefcaseSpeakers = 1749;
     
     private static boolean       isInitialized       = false;
     
@@ -22,8 +23,11 @@ public class ConfigHandler
             
             config.load();
             
+            // Blocks
+            idBriefcaseSpeakers = config.getBlock("id.BriefcaseSpeakers", idBriefcaseSpeakers).getInt();
+            
+            // Items
             idThiefGloves = config.getItem("id.ThiefGloves", idThiefGloves).getInt();
-            idBriefcaseSpeakers = config.getItem("id.BriefcaseSpeakers", idBriefcaseSpeakers).getInt();
             idSpeaker = config.getItem("id.Speaker", idSpeaker).getInt();
             idRemote = config.getItem("id.Remote", idRemote).getInt();
             idShadowIngot = config.getItem("id.ShadowIngot", idShadowIngot).getInt();
