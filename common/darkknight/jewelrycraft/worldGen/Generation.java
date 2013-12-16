88f9db07874f704b1dee719576c204b10e681577
bspkrs <bspkrs@gmail.com>
2013-12-16 11:28:15 -0500

    switched to bit shifting

--- common/darkknight/jewelrycraft/worldGen/Generation.java
+++ common/darkknight/jewelrycraft/worldGen/Generation.java
@@ -2,7 +2,6 @@ package darkknight.jewelrycraft.worldGen;
 
 import java.util.Random;
 
-
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
@@ -11,35 +10,37 @@ import darkknight.jewelrycraft.block.BlockList;
 
 public class Generation implements IWorldGenerator
 {
-
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
     {
-        switch(world.provider.dimensionId){
+        switch (world.provider.dimensionId)
+        {
             case -1:
-                generateNether(world, random, chunkX * 16, chunkZ * 16);
+                generateNether(world, random, chunkX << 4, chunkZ << 4);
                 break;
             case 0:
-                generateSurface(world, random, chunkX * 16, chunkZ * 16);
+                generateSurface(world, random, chunkX << 4, chunkZ << 4);
                 break;
             case 1:
-                generateEnd(world, random, chunkX * 16, chunkZ * 16);
+                generateEnd(world, random, chunkX << 4, chunkZ << 4);
                 break;
-            }        
+        }
     }
     
-    private void generateEnd(World world, Random random, int i, int j) {}
-
-    private void generateSurface(World world, Random random, int i, int j) 
+    private void generateEnd(World world, Random random, int i, int j)
+    {}
+    
+    private void generateSurface(World world, Random random, int i, int j)
     {
-        for(int k = 0; k < 1; k++) {
+        for (int k = 0; k < 1; k++)
+        {
             int x = i + random.nextInt(16);
             int y = random.nextInt(5);
             int z = j + random.nextInt(16);
             (new WorldGenMinable(BlockList.shadowOre.blockID, 1)).generate(world, random, x, y, z);
         }
     }
-
-    private void generateNether(World world, Random random, int i, int j) {}
-
+    
+    private void generateNether(World world, Random random, int i, int j)
+    {}
 }
