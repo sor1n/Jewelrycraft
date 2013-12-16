24ff7e4f3c399a3b23c45dbdef8829d57084e443
bspkrs <bspkrs@gmail.com>
2013-12-16 10:07:16 -0500

    added code to handle adding the metal to the ring

--- common/darkknight/jewelrycraft/block/BlockMolder.java
+++ common/darkknight/jewelrycraft/block/BlockMolder.java
@@ -2,142 +2,157 @@ package darkknight.jewelrycraft.block;
 
 import java.util.Random;
 
-import darkknight.jewelrycraft.config.ConfigHandler;
-import darkknight.jewelrycraft.item.ItemList;
-import darkknight.jewelrycraft.item.ItemRing;
-import darkknight.jewelrycraft.tileentity.TileEntityMolder;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import darkknight.jewelrycraft.item.ItemList;
+import darkknight.jewelrycraft.item.ItemRing;
+import darkknight.jewelrycraft.tileentity.TileEntityMolder;
 
 public class BlockMolder extends BlockContainer
 {
     Random rand = new Random();
+    
     protected BlockMolder(int par1, Material par2Material)
     {
         super(par1, par2Material);
         this.setBlockBounds(0.1F, 0F, 0.1F, 0.9F, 0.2F, 0.9F);
     }
-
+    
     @Override
     public TileEntity createNewTileEntity(World world)
     {
         return new TileEntityMolder();
     }
-
+    
     @Override
     public boolean renderAsNormalBlock()
     {
         return false;
     }
-
+    
     @Override
     public boolean onBlockActivated(World world, int i, int j, int k, EntityPlayer entityPlayer, int par6, float par7, float par8, float par9)
     {
         TileEntityMolder te = (TileEntityMolder) world.getBlockTileEntity(i, j, k);
         ItemStack item = entityPlayer.inventory.getCurrentItem();
-        if(te != null && item != null && !te.hasMold && item.itemID == ItemList.molds.itemID)
+        if (te != null && item != null && !te.hasMold && item.itemID == ItemList.molds.itemID)
         {
             te.mold = item;
             te.hasMold = true;
             --item.stackSize;
         }
-        if(te != null && te.hasMold && entityPlayer.isSneaking())
+        if (te != null && te.hasMold && entityPlayer.isSneaking())
         {
             entityPlayer.inventory.addItemStackToInventory(new ItemStack(te.mold.itemID, 1, te.mold.getItemDamage()));
             te.mold = new ItemStack(0, 0, 0);
             te.hasMold = false;
-        }   
+        }
         return true;
     }
-
-    public void onBlockDestroyedByPlayer(World par1World, int i, int j, int k, int par5) 
+    
+    @Override
+    public void onBlockDestroyedByPlayer(World par1World, int i, int j, int k, int par5)
     {
         TileEntityMolder te = (TileEntityMolder) par1World.getBlockTileEntity(i, j, k);
-        if(te != null) 
-        {            
+        if (te != null)
+        {
             float f = this.rand.nextFloat() * 0.8F + 0.1F;
             float f1 = this.rand.nextFloat() * 0.8F + 0.1F;
             float f2 = this.rand.nextFloat() * 0.8F + 0.1F;
-            if(te.hasMold)
+            if (te.hasMold)
             {
-                EntityItem entityitem = new EntityItem(par1World, (double)((float)i + f), (double)((float)j + f1), (double)((float)k + f2), new ItemStack(te.mold.itemID, 1, te.mold.getItemDamage()));
-
+                EntityItem entityitem = new EntityItem(par1World, i + f, j + f1, k + f2, new ItemStack(te.mold.itemID, 1, te.mold.getItemDamage()));
+                
                 if (te.mold.hasTagCompound())
                 {
-                    entityitem.getEntityItem().setTagCompound((NBTTagCompound)te.mold.getTagCompound().copy());
+                    entityitem.getEntityItem().setTagCompound((NBTTagCompound) te.mold.getTagCompound().copy());
                 }
-
+                
                 float f3 = 0.05F;
-                entityitem.motionX = (double)((float)this.rand.nextGaussian() * f3);
-                entityitem.motionY = (double)((float)this.rand.nextGaussian() * f3 + 0.2F);
-                entityitem.motionZ = (double)((float)this.rand.nextGaussian() * f3);
+                entityitem.motionX = (float) this.rand.nextGaussian() * f3;
+                entityitem.motionY = (float) this.rand.nextGaussian() * f3 + 0.2F;
+                entityitem.motionZ = (float) this.rand.nextGaussian() * f3;
                 par1World.spawnEntityInWorld(entityitem);
             }
-            if(te.hasJewelBase)
+            if (te.hasJewelBase)
             {
-                EntityItem entityitem = new EntityItem(par1World, (double)((float)i + f), (double)((float)j + f1), (double)((float)k + f2), new ItemStack(te.jewelBase.itemID, 1, te.jewelBase.getItemDamage()));
-
+                EntityItem entityitem = new EntityItem(par1World, i + f, j + f1, k + f2, new ItemStack(te.jewelBase.itemID, 1, te.jewelBase.getItemDamage()));
+                
                 if (te.jewelBase.hasTagCompound())
                 {
-                    entityitem.getEntityItem().setTagCompound((NBTTagCompound)te.jewelBase.getTagCompound().copy());
+                    entityitem.getEntityItem().setTagCompound((NBTTagCompound) te.jewelBase.getTagCompound().copy());
                 }
-
+                
                 float f3 = 0.05F;
-                entityitem.motionX = (double)((float)this.rand.nextGaussian() * f3);
-                entityitem.motionY = (double)((float)this.rand.nextGaussian() * f3 + 0.2F);
-                entityitem.motionZ = (double)((float)this.rand.nextGaussian() * f3);
+                entityitem.motionX = (float) this.rand.nextGaussian() * f3;
+                entityitem.motionY = (float) this.rand.nextGaussian() * f3 + 0.2F;
+                entityitem.motionZ = (float) this.rand.nextGaussian() * f3;
                 par1World.spawnEntityInWorld(entityitem);
             }
         }
     }
     
-    public void onBlockDestroyedByExplosion(World world, int i, int j, int k, Explosion par5Explosion) 
+    @Override
+    public void onBlockDestroyedByExplosion(World world, int i, int j, int k, Explosion par5Explosion)
     {
         onBlockDestroyedByPlayer(world, i, j, k, 0);
     }
     
     public void giveJewelToPlayer(TileEntityMolder md, EntityPlayer player, ItemStack item, ItemStack metal)
     {
-        player.inventory.addItemStackToInventory(item);
+        if (item != null)
+        {
+            if (item.itemID == ItemList.ring.itemID && metal != null)
+            {
+                Item r = new ItemRing(ItemList.ring.itemID, metal);
+                item = new ItemStack(r);
+            }
+            player.inventory.addItemStackToInventory(item);
+        }
     }
-
-    public void onBlockClicked(World world, int i, int j, int k, EntityPlayer player) 
+    
+    @Override
+    public void onBlockClicked(World world, int i, int j, int k, EntityPlayer player)
     {
-        TileEntityMolder me = (TileEntityMolder) world.getBlockTileEntity(i, j, k); 
-        if(me != null && me.hasJewelBase)
+        TileEntityMolder me = (TileEntityMolder) world.getBlockTileEntity(i, j, k);
+        if (me != null && me.hasJewelBase)
         {
             giveJewelToPlayer(me, player, me.jewelBase, me.ringMetal);
             me.jewelBase = new ItemStack(0, 0, 0);
-            me.hasJewelBase = false;            
-        }        
+            me.hasJewelBase = false;
+        }
     }
-
+    
+    @Override
     public boolean shouldSideBeRendered(IBlockAccess iblockaccess, int i, int j, int k, int l)
     {
         return false;
     }
-
+    
+    @Override
     public boolean isOpaqueCube()
     {
         return false;
     }
-
+    
     @Override
     public int getRenderType()
     {
         return -1;
     }
-
-    public void registerIcons(IconRegister icon) 
+    
+    @Override
+    public void registerIcons(IconRegister icon)
     {
         this.blockIcon = icon.registerIcon("jewelrycraft:molder");
     }
