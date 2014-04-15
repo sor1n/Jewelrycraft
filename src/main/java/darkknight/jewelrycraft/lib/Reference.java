2b4aedb9fb90e02c8ec89130919fb9cfae82770c
OnyxDarkKnight <sor1n.iliutza16@gmail.com>
2014-04-15 23:09:12 +0300

    removed the techne models from the resources folder

--- src/main/java/darkknight/jewelrycraft/lib/Reference.java
+++ src/main/java/darkknight/jewelrycraft/lib/Reference.java
@@ -4,6 +4,6 @@ public class Reference
 {
     public static final String MODID          = "Jewelrycraft";
     public static final String MODNAME        = "Jewelrycraft";
-    public static final String VERSION        = "1.3.2";
+    public static final String VERSION        = "1.3.3";
     public static final String PACKET_CHANNEL = "Jewelrycraft";
 }
