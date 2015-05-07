f03fa3703bea835a12a19bd42965af054540ea58
OnyxDarkKnight <sor1n.iliutza16@gmail.com>
2015-05-07 13:55:09 +0100

    Forgot to remove the guide from being a jewelry XD

--- src/main/java/darkknight/jewelrycraft/item/ItemGuide.java
+++ src/main/java/darkknight/jewelrycraft/item/ItemGuide.java
@@ -12,7 +12,7 @@ import net.minecraftforge.event.entity.player.PlayerEvent.Clone;
 import darkknight.jewelrycraft.JewelrycraftMod;
 import darkknight.jewelrycraft.api.IJewelryItem;
 
-public class ItemGuide extends Item implements IJewelryItem
+public class ItemGuide extends Item
 {
     public ItemGuide()
     {
@@ -25,55 +25,4 @@ public class ItemGuide extends Item implements IJewelryItem
         if (world.isRemote) player.openGui(JewelrycraftMod.instance, 1, player.worldObj, 0, 0, 0);
         return stack;
     }
-
-    /**
-     * @return
-     */
-    @Override
-    public int type()
-    {
-        return 0;
-    }
-
-    /**
-     * @param player
-     */
-    @Override
-    public void onWearAction(EntityPlayer player)
-    {
-        player.addPotionEffect(new PotionEffect(Potion.fireResistance.id, 20, 0));
-    }
-
-    /**
-     * @param player
-     * @param source
-     * @param amount
-     */
-    @Override
-    public void onPlayerAttackedAction(EntityPlayer player, DamageSource source, float amount)
-    {}
-
-    /**
-     * @param player
-     * @param entity
-     * @param amount
-     */
-    @Override
-    public void onEntityAttackedByPlayer(EntityPlayer player, EntityLivingBase entity, float amount)
-    {}
-
-    /**
-     * @param player
-     * @param source
-     */
-    @Override
-    public void onPlayerDeadAction(EntityPlayer player, DamageSource source)
-    {}
-
-    /**
-     * @param event
-     */
-    @Override
-    public void onPlayerRespawnAction(Clone event)
-    {}
 }
